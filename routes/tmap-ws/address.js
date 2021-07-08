0cab7ade1790a855f3ba3f9ac817bc775392b1ef
Karshilov <karshilov@foxmail.com>
2021-07-08 17:55:33 +0800

    address

---
+++ routes/tmap-ws/address.js
@@ -0,0 +1,13 @@
+const apiKey = 'B2EBZ-YEB6Q-WVF55-GBMOS-R5YV7-GGFPE'
+
+exports.route = {
+  async get(info) {
+    const { addr } = info
+    const res = await this.get(`https://apis.map.qq.com/ws/geocoder/v1/?address=${encodeURIComponent(addr)}&key=${apiKey}`)
+    if (res.data.status === 0) {
+      return res.data.result
+    } else {
+      throw res.data.message
+    }
+  },
+}
