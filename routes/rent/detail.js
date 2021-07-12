524e7d029fea6e254092fedbfc0812891b5326f2
GavinCmy <1141159857@qq.com>
2021-07-12 11:35:27 +0800

    修改了detail.js

--- routes/rent/detail.js
+++ routes/rent/detail.js
@@ -29,6 +29,7 @@ exports.route = {
     }
     return record
   },
+
   async post({title, photos, area, floor, total_floor, 
     price, house_type, decoration, features, neighbourhood, 
     city, rent_type, equipments}) {
@@ -50,5 +51,54 @@ exports.route = {
       throw '添加失败'
     }
     return '添加成功'
+  },
+
+  async put({id,title, photos, area, floor, total_floor, 
+    price, house_type, decoration, features, neighbourhood, 
+    city, rent_type, equipments}) {
+    let now = moment().format('X')
+    photos = photos.toString()
+    features = features.toString()
+    try {
+      await this.db.query(`
+        UPDATE ESTATE_RENT_DETAIL
+        SET 
+          TITLE = $1, 
+          PHOTOS = $2, 
+          AREA = $3, 
+          FLOOR = $4, 
+          TOTAL_FLOOR = $5,
+          PRICE = $6, 
+          OWNER = $7, 
+          HOUSE_TYPE = $8, 
+          DECORATION = $9, 
+          FEATURES = $10, 
+          NEIGHBOURHOOD = $11,
+          CITY = $12, 
+          LAST_MODIFY_TIME = $13, 
+          RENT_TYPE = $14, 
+          EQUIPMENTS = $15       
+        WHERE   
+          ID = $16
+      `, [title, photos, area,  floor, total_floor, price, this.user.id, house_type, 
+        decoration, features, neighbourhood, city, now, rent_type, equipments, id])
+    } catch (e) {
+      console.log(e)
+      throw '修改失败'
+    }
+    return '修改成功'
+  },
+
+  async delete({id}) {
+    try {
+      await this.db.query(`
+        DELETE FROM ESTATE_RENT_DETAIL
+        WHERE ID = $1
+      `, [id])
+    } catch (e) {
+      console.log(e)
+      throw '删除失败'
+    }
+    return '删除成功'
   }
 }
