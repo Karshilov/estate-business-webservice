d362de6f659ce20357d0b8d75e6714b162ae827c
unconsolable <chenzhipeng2012@gmail.com>
2021-07-13 15:02:51 +0800

    修改搜索参数名

--- routes/rent/search.js
+++ routes/rent/search.js
@@ -1,16 +1,16 @@
 exports.route = {
-  async get({city, neighborhood, page_size = 10, page_num = 1}) {
-    if (!city || !neighborhood) {
+  async get({city, neighbourhood, page_size = 10, page_num = 1}) {
+    if (!city || !neighbourhood) {
       throw '缺少参数'
     }
-    neighborhood = '%' + neighborhood + '%'
+    neighbourhood = '%' + neighbourhood + '%'
     let records, total
     try {
       let cntRecords = await this.db.query(`
         SELECT COUNT(*)
         FROM ESTATE_RENT_DETAIL
-        WHERE CITY = $1 AND NEIGHBOURHOOD LIKE $2 AND status = $3
-      `, [city, neighborhood, 'approve'])
+        WHERE CITY = $1 AND NEIGHBOURHOOD LIKE $2
+      `, [city, neighbourhood])
 
       total = parseInt(cntRecords.rows[0].count)
       records = await this.db.query(`
@@ -18,7 +18,7 @@ exports.route = {
         FROM ESTATE_RENT_DETAIL
         WHERE CITY = $1 AND NEIGHBOURHOOD LIKE $2
         LIMIT $3 OFFSET $4
-      `, [city, neighborhood, page_size, (page_num - 1) * page_size])
+      `, [city, neighbourhood, page_size, (page_num - 1) * page_size])
     } catch (e) {
       console.log(e)
       throw '数据库异常'
