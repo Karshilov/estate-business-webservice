5a8047f327f4e5ddba136f3c9af6a7f82850b8d7
unconsolable <chenzhipeng2012@gmail.com>
2021-07-21 11:13:21 +0800

    完善用户中心-预约查看

--- routes/user/appointment.js
+++ routes/user/appointment.js
@@ -1,19 +1,47 @@
 exports.route = {
-  async get({id}) {
+  async get({id, page_num, page_size}) {
     if(!id){//不填id 默认查看用户自己的预约信息
       id = this.user.id
+    }
+    let cnt = await this.db.query(`
+      SELECT COUNT(*)
+      FROM ESTATE_APPOINTMENT
+      WHERE USER_ID = $1
+    `, [id])
+    if (cnt.rows.length === 0) {
+      return {total: 0, list: []}
     }
     let houseID
     try {
       houseID = await this.db.query(`
-        SELECT HOUSE_ID
+        SELECT HOUSE_ID, APPOINTMENT_TIME, CREATE_TIME, MODIFY_TIME
         FROM ESTATE_APPOINTMENT
         WHERE USER_ID = $1
-      `, [id])
+        LIMIT $2 OFFSET $3
+      `, [id, page_size, (page_num-1)*page_size])
     } catch (e) {
       throw '数据库异常'
     }
-    return {IDs:houseID.rows}
+    let results = []
+    for (let {house_id, appointment_time, create_time, modify_time} of houseID.rows) {
+      let record = await this.db.query(`
+        SELECT ID, TITLE, PHOTOS, AREA, FEATURES
+        FROM ESTATE_RENT_DETAIL
+        WHERE ID = $1
+      `, [house_id])
+      record.rows[0].features = record.rows[0].features.split(',')
+      if (!record.rows[0].photos) {
+        record.rows[0].cover = await this.genGetURL('house', 'foobar1.jpg')
+      } else {
+        record.rows[0].cover = await this.genGetURL('house', record.rows[0].photos.split(',')[0])
+      }
+      record.rows[0].photos = undefined
+      record.rows[0].appointment_time = appointment_time
+      record.rows[0].create_time = create_time
+      record.rows[0].modify_time = modify_time
+      results.push(record.rows[0])
+    }
+    return {total: cnt.rows[0].count, list: results}
   },
   async post({house_id, appointment_time}) {
     let now = moment().format('X')
