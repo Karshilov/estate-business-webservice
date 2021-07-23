ac930177146cfdfebf92af16f42b4fca02a67a52
unconsolable <chenzhipeng2012@gmail.com>
2021-07-23 18:11:10 +0800

    user中增加团队信息返回

--- routes/user/index.js
+++ routes/user/index.js
@@ -21,6 +21,23 @@ exports.route = {
     if (id !== this.user.id) {
       ret.email = ret.phone_number = undefined
     }
+    ret.team = await this.userHelper.getTeamById(id)
+    if (ret.team) {
+      ret.team.leader = await this.userHelper.getUserById(ret.team.leader_id)
+      ret.team.leader.userid = ret.team.leader_id
+      ret.team.leader.avatar = await this.genGetURL('avatar', ret.team.leader.avatar)
+      ret.team.leader.email = undefined
+      ret.team.leader.phone_number = undefined
+      ret.team.leader_id = undefined
+      for (let i = 0; i < ret.team.member_ids.length; i++) {
+        let id = ret.team.member_ids[i]
+        ret.team.member_ids[i] = await this.userHelper.getUserById(id)
+        ret.team.member_ids[i].userid = id
+        ret.team.member_ids[i].avatar = await this.genGetURL('avatar', ret.team.member_ids[i].avatar)
+        ret.team.member_ids[i].email = undefined
+        ret.team.member_ids[i].phone_number = undefined
+      }
+    }
     return ret
   },
   async put({username, nickname, email, gender, phone_number}) {
